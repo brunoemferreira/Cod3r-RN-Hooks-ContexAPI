b6a9bb9d718b6e19348ab8ba738ccb291ab150c7
brunoemferreira <brunoemf@gmail.com>
2021-03-29 00:47:25 -0300

    Creating App navigation, linking React Native Vector icons and creating UserForm view

--- rncrud/src/App.js
+++ rncrud/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Button, Icon } from 'react-native-elements'
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
 
@@ -9,16 +10,43 @@ const Stack = createStackNavigator()
 export default props => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
         <Stack.Screen
           name="UserList"
           component={UserList}
+          options={({ navigation }) => {
+            return {
+              title: "Lista de Usuários",
+              headerRight: () => (
+                < Button
+                  onPress={() => navigation.navigate("UserForm")}
+                  type="clear"
+                  icon={< Icon name="add" size={25} color="white" />}
+                />
+              )
+            }
+          }}
         />
         <Stack.Screen
           name="UserForm"
           component={UserForm}
+          options={{
+            title: "Formulário de Usuários"
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   )
+}
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e'
+  },
+  headerTintColor: '#ffffff',
+  headerTitleStyle: {
+    fontWeight: 'bold'
+  }
 }
