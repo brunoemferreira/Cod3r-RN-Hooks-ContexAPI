95a8e9198803a2098b3b4002da5d240440baeca7
brunoemferreira <brunoemf@gmail.com>
2021-03-29 21:04:55 -0300

    Creating users file

---
+++ rncrud/src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: 'João da Silva',
+    email: 'joaodasilva@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448_960_720.png'
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar',
+    email: 'mariana@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/12/16/34/vampire-151178_960_720.png'
+  },
+  {
+    id: 3,
+    name: 'Julio Fragoso',
+    email: 'julio@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png'
+  },
+  {
+    id: 4,
+    name: 'Antonio Roberto',
+    email: 'aroberto@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  }
+]
