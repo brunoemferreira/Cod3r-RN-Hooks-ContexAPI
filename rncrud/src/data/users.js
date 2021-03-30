345bf5e070daa0c0bc517b4fcb8d2a71a7204390
brunoemferreira <brunoemf@gmail.com>
2021-03-29 21:33:16 -0300

    Creating User List and users data file

--- rncrud/src/data/users.js
+++ rncrud/src/data/users.js
@@ -1,26 +1,30 @@
 export default [
   {
     id: 1,
-    name: 'João da Silva',
-    email: 'joaodasilva@email.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448_960_720.png'
+    name: 'João Silva',
+    email: 'josil@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
   },
   {
     id: 2,
     name: 'Mariana Aguiar',
-    email: 'mariana@email.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/12/16/34/vampire-151178_960_720.png'
+    email: 'riamasil@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png',
   },
   {
     id: 3,
     name: 'Julio Fragoso',
-    email: 'julio@email.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png'
+    email: 'juliafral@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
   },
   {
     id: 4,
-    name: 'Antonio Roberto',
-    email: 'aroberto@email.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
-  }
-]
+    name: 'Rafael Monteiro',
+    email: 'rafamontee@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png',
+  },
+];
--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { Text } from 'react-native';
+
+import React, { useContext } from 'react'
+import { View, FlatList, Alert } from 'react-native'
+import { ListItem, Button, Icon } from 'react-native-elements'
+import users from '../data/users';
+
 
 export default props => {
+
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        leftAvatar={{ source: { uri: user.avatarUrl } }}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm', user)}
+      />
+    )
+  }
+
   return (
-    <Text>UserList</Text>
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
